006f853d04e01ac5b7de5913a2d158f485c94f7c
NSANZUMUHIRE FIDELE <fidelensanze100@gmail.com>
2025-07-26 01:13:08 +0200

    Add celery setting

--- Umuganda/celery.py
+++ Umuganda/celery.py
@@ -1,6 +1,4 @@
-
 import os
-import ssl
 from celery import Celery
 from celery.schedules import crontab
 from django.conf import settings
@@ -11,12 +9,6 @@ app = Celery("Umuganda")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.conf.timezone = 'Africa/Kigali'
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-app.autodiscover_tasks()
-
-# Add Redis SSL config for Upstash (or any rediss:// broker)
-app.conf.broker_use_ssl = {
-    'ssl_cert_reqs': ssl.CERT_NONE,  # or ssl.CERT_REQUIRED if you have proper certs
-}
 
 app.conf.beat_schedule = {
     'send-umuganda-notifications-twice-daily': {
